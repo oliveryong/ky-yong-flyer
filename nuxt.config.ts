47ce093c5c3cce59f394e13e4406d60dbd486542
oliverYWC <oliver.ywc86@gmail.com>
2025-07-08 20:32:29 +0800

    set up all dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
 
   modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/ui'
-  ]
-})
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/ui",
+  ],
+  css: ["~/assets/css/main.css"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  icon: {
+    customCollections: [
+      {
+        prefix: "custom",
+        dir: "./assets/icons",
+      },
+    ],
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <UButton>Test</UButton>
+  </div>
+</template>
+
+<script setup>
+onMounted(() => {
+  console.log("test");
+});
+</script>
